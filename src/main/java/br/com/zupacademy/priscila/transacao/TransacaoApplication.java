72baca3d396f5e1978f6eb80c4c06fe40adc7d4d
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-25 14:07:10 -0300

    Alterando o deserializer do Kafka

--- src/main/java/br/com/zupacademy/priscila/transacao/TransacaoApplication.java
+++ src/main/java/br/com/zupacademy/priscila/transacao/TransacaoApplication.java
@@ -2,7 +2,8 @@ package br.com.zupacademy.priscila.transacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
 
 import java.util.TimeZone;
 
@@ -12,6 +13,11 @@ public class TransacaoApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(TransacaoApplication.class, args);
+	}
+
+	@Bean
+	JsonMessageConverter jsonMessageConverter(){
+		return new JsonMessageConverter();
 	}
 
 }
